6ca607586c99ad1b21951f156dd9ac36de6a0386
Peaches <shinasaki07@gmail.com>
2018-07-25 17:08:53 +0700

    :bug: test rsync.

--- docker-entrypoint-initdb.d/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-# docker-entrypoint.sh
-"${mongo[@]}" "$rootAuthDatabase" ;-EOJS
-	db.createUser({
-		user: $(jq --arg 'user' "$MONGO_INITDB_ROOT_USERNAME" --null-input '$user'),
-		pwd: $(jq --arg 'pwd' "$MONGO_INITDB_ROOT_PASSWORD" --null-input '$pwd'),
-		roles: [ { role: 'root', db: $(jq --arg 'db' "$rootAuthDatabase" --null-input '$db') } ]
-	})
-EOJS
--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-# docker-entrypoint.sh
-for f in /docker-entrypoint-initdb.d/*; do
-	case "$f" in
-		*.sh) echo "$0: running $f"; . "$f" ;;
-		*.js) echo "$0: running $f"; "${mongo[@]}" "$MONGO_INITDB_DATABASE" "$f"; echo ;;
-		*)    echo "$0: ignoring $f" ;;
-	esac
-	echo
-done
